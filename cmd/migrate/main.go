00c8d6669a4e42f8077af73789e7d3cf7b240ad3
DerrickKirimi <remmydren@gmail.com>
2023-03-10 12:01:18 +0300

    Goose Migrations

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -57,3 +57,33 @@ func main () {
 		log.Fatalf("migrate run: %v", err)
 	}
 }
+
+func usage() {
+			fmt.Println(usagePrefix)
+			flags.PrintDefaults()
+			fmt.Println(usageCommands)
+}
+
+var (
+		usagePrefix = `Usage: migrate [OPTIONS] COMMAND
+Examples:
+	migrate status
+Options:
+`
+
+		usageCommands = `
+Commands:
+	up					Migrate the DB to the most recent version available
+	up-by-one 			Migrate the DB up by 1
+	up-to-version		Migrate the DB to a specific VERSION
+	down 				Roll back to version 1
+	down-to-version		Roll back to a specific VERSION
+	redo                 Re-run the latest migration
+    reset                Roll back all migrations
+    status               Dump the migration status for the current DB
+    version              Print the current version of the database
+    create NAME [sql|go] Creates new migration file with the current timestamp
+    fix                  Apply sequential ordering to migrations
+`
+
+)
